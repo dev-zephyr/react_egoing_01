77423cee9b2be86b1ffca349095edb1a3c5fac9f
zephyr <bbang1424@naver.com>
2021-02-17 14:15:18 +0900

    day 03

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TOC from './components/TOC';
 import Subject from './components/Subject';
 import ReadContent from './components/ReadContent';
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Control from './components/Control';
 import { render } from '@testing-library/react';
 
@@ -13,7 +14,7 @@ class App extends Component {
     super(props)
     this.max_content_id = 3;
     this.state = {
-      mode : 'read',
+      mode : 'welcome',
       selected_content_id : 2,
       welcome : {title : 'Welcome', desc : 'Hello, React!!'},
       subject : {title : 'WEB', sub : 'world wide WEB!'},
@@ -24,33 +25,81 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    let _title, _desc, _article = null;
+
+  getReadContent(){
+    for(var i = 0; i<this.state.contents.length; i++) {
+      if(this.state.contents[i].id === this.state.selected_content_id) {
+        return this.state.contents[i];
+      }
+    }
+  }
+
+  getContent() {
+    let _title, _desc, _article, _content = null;
+
     if (this.state.mode === 'welcome') {
+
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+
     } else if (this.state.mode === 'read') {
-      for(var i = 0; i<this.state.contents.length; i++) {
-        if(this.state.contents[i].id === this.state.selected_content_id) {
-          _title = this.state.contents[i].title;
-          _desc = this.state.contents[i].desc;
-        }
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+
+      _content = this.getReadContent();
+      console.log("=================" , _content);
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+
     } else if(this.state.mode === 'create') {
+
       _article = <CreateContent onSubmit={(_title, _desc) => {
+        
         let _contents = this.state.contents.concat({
           id : ++this.max_content_id,
           title : _title,
           desc : _desc
         })
+
         this.setState({
           contents : _contents
         })
-        
+
       }}></CreateContent>
+
+    } else if(this.state.mode === 'update') {
+
+      console.log("update===========" , this.getReadContent());
+
+      _article = <UpdateContent 
+        data={this.getReadContent()}
+        onSubmit={(_id, _title, _desc) => {
+          
+          let _contents = Array.from(this.state.contents);
+          console.log("수정 전", _contents);
+          
+          for(let i = 0; i<_contents.length; i++) {
+            if(_contents[i].id === _id) {
+              _contents[i] = {
+                id : _id,
+                title : _title,
+                desc : _desc
+              }
+              console.log("수정 후", _contents);
+              this.setState({
+                contents : _contents
+              })
+              break;
+            }
+          }
+        
+        }}>
+
+      </UpdateContent>
     }
+    return _article;
+  }
+
+  render() {
+    
     return (
       <div className="App">
         <Subject 
@@ -88,12 +137,29 @@ class App extends Component {
         <hr />
         <Control
           onChangeMode={function(_mode){
-            this.setState({
-              mode : _mode
-            })
+            if(_mode === 'delete') {
+              
+              if(window.confirm('삭제하시겠습니까?')) {
+                
+                for(let i=0; i<this.state.contents.length; i++) {
+                  if(this.state.contents[i].id === this.state.selected_content_id) {
+                    this.state.contents.splice(i, 1);
+                    break;
+                  }
+                }
+                this.setState({
+                  mode : 'welcome'
+                })
+                alert('삭제 완료');
+              }
+            } else {
+              this.setState({
+                mode : _mode
+              })
+            }
           }.bind(this)}></Control>
         <hr />
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
---
+++ src/components/UpdateContent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class UpdateContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id : this.props.data.id,
+      title : this.props.data.title,
+      desc : this.props.data.desc
+    }
+  }
+
+  inputFormHandler(e){
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  render() {
+    console.log("UpdateContent render");
+    return (
+        <article>
+          <h2>Update</h2>
+          <form action="/create_process" method="post" 
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  this.props.onSubmit(
+                    this.state.id,
+                    this.state.title,
+                    this.state.desc
+                  );
+                }}> 
+            <input type="hidden" name="id" value={this.state.id}></input>
+            <p>
+              <input type="text" name="title" value={this.state.title}
+                onChange={(e) => {
+                  this.inputFormHandler(e);
+                }}></input>
+            </p>
+            <p>
+              <textarea name="desc" value={this.state.desc}
+                onChange={(e) => {
+                  this.inputFormHandler(e);
+                }}></textarea>
+            </p>
+            <p>
+              <input type="submit"></input>
+            </p>
+
+          </form>
+        </article>
+    );
+  }
+}
+
+export default UpdateContent;
